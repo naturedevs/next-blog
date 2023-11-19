637bab8c62de31f5970937044f38f90320f288a6
Jay <140168334+Qdev1226@users.noreply.github.com>
2023-11-19 14:39:43 +0200

    Edit package.json engine

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>[Hello, I am Harry Julin]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
